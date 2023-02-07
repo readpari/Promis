a570f0045567750648b8f1ae205e11b3b10fe2b7
Evgeny <e.litvinenko@mute-lab.com>
2023-02-07 16:45:23 +0400

    refactor and implement reader load

--- frontend/components/Read/index.jsx
+++ frontend/components/Read/index.jsx
@@ -1,7 +1,7 @@
 import { async } from 'regenerator-runtime';
 import { clear } from 'console';
 import 'regenerator-runtime/runtime';
-import React from 'react';
+import React, {useCallback, useMemo, useRef} from 'react';
 import { useState, useEffect } from 'react';
 import { Routes, Route, Navigate } from 'react-router-dom';
 
@@ -10,16 +10,41 @@ import { Routes, Route, Navigate } from 'react-router-dom';
 export default function Read({ isSignedIn, guestBook, wallet }) {
   const [readOn, setReadOn] = useState(false);
   const [readTimer, setReadTimer] = useState(0);
+  const startButtonRef = useRef();
+
   const read = async () => {
     setReadTimer((readTimer) => readTimer + 1);
     await guestBook.timer();
   };
 
+  const createBookElementInPage = useCallback(() => {
+    const div = document.createElement('div' )
+    div.id = 'area'
+
+    if(!startButtonRef.current){
+      return;
+    }
+
+    document.querySelector('.container').insertAdjacentElement('beforeend', div);
+
+
+  }, [startButtonRef])
+
+  const createBookInstance = useCallback(() => {
+    const book = ePub('https://s3.amazonaws.com/moby-dick/OPS/package.opf');
+    const rendition = book.renderTo('area', {
+      height: "75vh",
+      manager: "continuous",
+      flow: "scrolled",
+      width: '100vw',
+      allowScriptedContent: true });
+    const displayed = rendition.display("epubcfi(/6/14[xchapter_001]!4/2/24/2[c001p0011]/1:799)");
+  }, [startButtonRef])
+
   useEffect(() => {
-    // var book = ePub('alice.epub');
-    // var rendition = book.renderTo('area', { width: 600, height: 400 });
-    // var displayed = rendition.display();
-  }, []);
+    createBookElementInPage()
+    createBookInstance()
+  }, [createBookElementInPage, createBookInstance]);
 
   useEffect(() => {
     const interval = setInterval(() => {
@@ -33,13 +58,9 @@ export default function Read({ isSignedIn, guestBook, wallet }) {
     };
   }, [readOn]);
 
-  var book = ePub('alice.epub');
-  var rendition = book.renderTo('area', { width: 600, height: 400 });
-  var displayed = rendition.display();
-  console.log(ePub);
   return (
     <>
-      <button onClick={() => setReadOn(!readOn)}>{readOn ? 'stop' : 'start'}</button>
+      <button ref={startButtonRef} onClick={() => setReadOn(!readOn)}>{readOn ? 'stop' : 'start'}</button>
     </>
   );
 }
